e2e46c2a053fadbcb3bece1beac25efe18f9a244
Ben Patterson <ben.patterson@jupyter4.ligo.caltech.edu>
2024-04-24 11:20:47 -0700

    shifted_e() now only returns array if array inputted

--- .ipynb_checkpoints/calcwf-checkpoint.py
+++ .ipynb_checkpoints/calcwf-checkpoint.py
@@ -351,6 +351,8 @@ def shifted_e(s_f, f, e):
     bounds = [(0, 0.999)]
     best_fit = minimize(lambda x: np.sum(abs(shifted_e_const(s_f, x)-constant)), init_guess, bounds=bounds)
     s_e = np.array(best_fit['x'])
+    if len(s_e) == 1:
+        s_e = s_e[0]
 
     return s_e
 
